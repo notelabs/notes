2a8bbc10c7be3ffd04f313533029422803813467
Jacob Marshall <jacob@omg.lol>
2022-04-18 15:40:08 +0100

    💬 Add a title

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,21 +1,27 @@
+import Head from "next/head";
 import { Hero, Nav } from "ui";
 
 export default function Web() {
   const links = [
     { name: "Dashboard", href: "/" },
-    { name: "Projects", href: "/" },
+    { name: "Notes", href: "/" },
     { name: "Team", href: "/" }
   ]
 
   return (
-    <div>
-      <Nav
-        links={links}
-      />
-      <Hero
-        heading="Take notes on the future of notetaking."
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-      />
-    </div>
+    <>
+      <Head>
+        <title>Take notes on the future of notetaking - Notelabs</title>
+      </Head>
+      <div>
+        <Nav
+          links={links}
+        />
+        <Hero
+          heading="Take notes on the future of notetaking."
+          description="Notelabs makes it easy to to write down your thoughts. Choose your devices, and let Notelabs seamlessly adept to them, letting you get back to your writing."
+        />
+      </div>
+    </>
   );
 }
--- packages/ui/Nav.tsx
+++ packages/ui/Nav.tsx
@@ -1,22 +1,23 @@
-import { ReactNode } from 'react';
 import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
+    Box,
+    Flex,
+    Avatar,
+    HStack,
+    Link,
+    IconButton,
+    Button,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    useDisclosure,
+    useColorModeValue,
+    Stack,
+    useColorMode,
+    Heading
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon, SunIcon, MoonIcon } from '@chakra-ui/icons';
 import { Fade } from '.';
 
 type NavProps = {
@@ -29,76 +30,78 @@ type NavLinkProps = {
 }
 
 const NavLink = ({ name, href }: NavLinkProps) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={href}>
-    {name}
-  </Link>
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={href}>
+        {name}
+    </Link>
 );
 
 export function Nav({ links }: NavProps) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { colorMode, toggleColorMode } = useColorMode()
 
-  return (
-    <Fade delay={1.6}>
-      <Box px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Box>Notelabs</Box>
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {links.map((link) => (
-                <NavLink key={link.href} name={link.name} href={link.href} />
-              ))}
-            </HStack>
-          </HStack>
-          <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}>
-                <Avatar
-                  size={'sm'}
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
-                <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
-              </MenuList>
-            </Menu>
-          </Flex>
-        </Flex>
+    return (
+        <Fade delay={1.6}>
+            <Box px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <HStack spacing={8} alignItems={'center'}>
+                        <Heading size="md" py={2}>Notelabs</Heading>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', md: 'flex' }}>
+                            {links.map((link) => (
+                                <NavLink key={link.href} name={link.name} href={link.href} />
+                            ))}
+                        </HStack>
+                    </HStack>
+                    <HStack alignItems={'center'} spacing={4}>
+                        <IconButton variant="ghost" rounded="full" aria-label='Toggle theme' onClick={toggleColorMode} icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} />
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                minW={0}>
+                                <Avatar
+                                    size={'sm'}
+                                />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Link 1</MenuItem>
+                                <MenuItem>Link 2</MenuItem>
+                                <MenuDivider />
+                                <MenuItem>Link 3</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </HStack>
+                </Flex>
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {links.map((link) => (
-                <NavLink key={link.href} name={link.name} href={link.href} />
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </Fade>
-  );
+                {isOpen ? (
+                    <Box pb={4} display={{ md: 'none' }}>
+                        <Stack as={'nav'} spacing={4}>
+                            {links.map((link) => (
+                                <NavLink key={link.href} name={link.name} href={link.href} />
+                            ))}
+                        </Stack>
+                    </Box>
+                ) : null}
+            </Box>
+        </Fade>
+    );
 }
