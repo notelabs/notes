43893706c46365580f7a17cd8f8a83369e8cd149
jacobhq <jacob@omg.lol>
2022-04-21 21:29:24 +0000

    ✨ Form validation

--- apps/notes/pages/auth/login.tsx
+++ apps/notes/pages/auth/login.tsx
@@ -15,6 +15,9 @@ import {
     Icon,
     Text,
     Divider,
+    FormErrorMessage,
+    FormHelperText,
+    Spacer,
 } from '@chakra-ui/react';
 import { Nav } from 'ui';
 import { HiBeaker, HiCollection, HiDeviceMobile, HiDocumentDownload, HiKey, HiLightningBolt, HiPhone } from 'react-icons/hi';
@@ -23,10 +26,10 @@ import { signIn } from "next-auth/react"
 import { useState } from 'react';
 import Head from 'next/head';
 import { FeatureList, list } from '../../lib/features';
+import { Formik, Form, Field } from 'formik';
 
 export default function Login() {
     let [isLoading, setLoading] = useState("")
-    let [email, setEmail] = useState("")
 
     function signInGithub() {
         setLoading("github")
@@ -38,6 +41,19 @@ export default function Login() {
         signIn("email", { callbackUrl: "/app", email })
     }
 
+    function validateEmail(value: string) {
+        let error
+        if (!value) {
+            error = 'Required';
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)
+        ) {
+            error = 'Invalid email address';
+        }
+        return error
+    }
+
+
     return (
         <>
             <Head>
@@ -46,16 +62,36 @@ export default function Login() {
             <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
                 <Flex p={8} flex={1} align={'center'} justify={'center'}>
                     <Stack spacing={4} w={'full'} maxW={'md'}>
-                        <Heading fontSize={'3xl'}>Sign in to your account</Heading>
+                        <Heading fontSize={'3xl'}>Sign in or sign up</Heading>
                         <Text opacity={0.75}>Enter your email to sign in with a magic link, or sign in with a provider.</Text>
-                        <FormControl id="email">
-                            <Input required type="email" placeholder='Enter an email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </FormControl>
-                        <Stack spacing={6}>
-                            <Button colorScheme={'blue'} variant={'solid'} isLoading={isLoading === "email"} onClick={() => signInEmail(email)}>
-                                Sign in
-                            </Button>
-                        </Stack>
+                        <Spacer />
+                        <Formik
+                            initialValues={{ email: '' }}
+                            onSubmit={(values, actions) => {
+                                setTimeout(() => {
+                                    signInEmail(values.email)
+                                    actions.setSubmitting(false)
+                                }, 1000)
+                            }}
+                        >
+                            {(props) => (
+                                <Form>
+                                    <Stack spacing={4}>
+                                        <Field name='email' validate={validateEmail}>
+                                            {({ field, form }: any) => (
+                                                <FormControl isInvalid={form.errors.email && form.touched.email}>
+                                                    <Input {...field} required type="email" placeholder='Enter an email' id='name' />
+                                                    {form.errors.email ? <FormErrorMessage>{form.errors.email}</FormErrorMessage> : <FormHelperText>No spam, emails once a quarter at most</FormHelperText>}
+                                                </FormControl>
+                                            )}
+                                        </Field>
+                                        <Button type="submit" isFullWidth colorScheme={'blue'} variant={'solid'} isLoading={isLoading === "email" || props.isSubmitting} isDisabled={props.values.email === '' || props.errors.email ? true : false}>
+                                            Sign in
+                                        </Button>
+                                    </Stack>
+                                </Form>
+                            )}
+                        </Formik>
                         <Divider py={3} />
                         <Button colorScheme="gray" leftIcon={<Icon as={FaGithub} />} onClick={signInGithub} isLoading={isLoading === "github"}>
                             Continue with GitHub
