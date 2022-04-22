73b22ffb2554eefa1534e44568ef6291456d97f2
Jacob Marshall <jacob@omg.lol>
2022-04-22 19:08:02 +0100

    ✨ Add update api routes

---
+++ apps/notes/pages/api/document/update/index.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import prisma from '../../../../lib/prisma'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req })
+    if (!session) return res.status(401).send("Unauthenticated")
+    if (!req.body.id || !req.body.content) return res.status(400).send("Missing id or content")
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email: session?.user?.email
+        }
+    })
+
+    const doc = await prisma.document.update({
+        where: {
+            id: req.body.id,
+            // @ts-ignore
+            owner: user.id
+        },
+        data: {
+            content: req.body.content
+        }
+    })
+}
---
+++ apps/notes/pages/api/document/update/title/index.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import prisma from '../../../../../lib/prisma'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req })
+    if (!session) return res.status(401).send("Unauthenticated")
+    if (!req.body.id || !req.body.title) return res.status(400).send("Missing id or content")
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email: session?.user?.email
+        }
+    })
+
+    const doc = await prisma.document.update({
+        where: {
+            id: req.body.id,
+            // @ts-ignore
+            owner: user.id
+        },
+        data: {
+            // @ts-ignore
+            title: req.body.title
+        }
+    })
+}
