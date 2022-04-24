3b7bd06d172148f6cb06969f3af66177e531371c
Jacob Marshall <jacob@omg.lol>
2022-04-24 19:29:11 +0100

    ✨ Edit support

--- apps/notes/pages/index.tsx
+++ apps/notes/pages/index.tsx
@@ -5,6 +5,7 @@ import { useColor } from "hooks"
 import { signIn, useSession } from "next-auth/react"
 import useSWR from "swr";
 import { ReactChild, ReactFragment, ReactPortal } from "react";
+import { formatDistance } from 'date-fns'
 
 export default function Notes() {
   let shadow = useColorModeValue("xs", "")
@@ -41,7 +42,7 @@ export default function Notes() {
       <Layout>
         <Container maxW="container.lg" mt={6}>
           <SimpleGrid columns={[1, null, 3]}>
-            {data && !error ? data.map((i: { summary: string; title: string; }) => (
+            {data && !error ? data.map((i: { summary: string; title: string; updatedAt: string; }) => (
               <Box p={5} borderWidth="1px" borderRadius={6}>
                 <Box transition="all 0.3s ease" borderRadius={6} boxShadow={shadow} _hover={{ boxShadow: hoverShadow }} p={5} mb={5}>{i.summary}</Box>
                 <Editable
@@ -57,7 +58,11 @@ export default function Notes() {
                     <EditableControls />
                   </HStack>
                 </Editable>
-                <Text as="small" color={secondaryColor}>Edited yesterday</Text>
+                <Text as="small" color={secondaryColor}>Edited {formatDistance(
+                  new Date(i.updatedAt),
+                  new Date(),
+                  { addSuffix: true }
+                )}</Text>
               </Box>
             )) : error ? "An error occured" : [...Array(5).keys()].map((i) => (
               <Box p={5} borderWidth="1px" borderRadius={6} key={i}>
