0f60e71fc91c5229670a6ed6edc7e9f029b67dff
Jacob Marshall <jacob@omg.lol>
2022-04-28 16:23:28 +0100

    💫 Finalise document create animation

--- apps/notes/pages/_app.tsx
+++ apps/notes/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { theme } from "config"
 import "cal-sans"
 import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
