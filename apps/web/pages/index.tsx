23d7ab37954ca4b5d8a5e6a76557b72563c995d8
Jacob Marshall <jacob@omg.lol>
2022-05-08 18:14:04 +0100

    🍱 Update img

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Web() {
         <meta name="twitter:site" content="@trynotelabs" />
         <meta name="twitter:title" content="Take notes on the future of notetaking - Notelabs" />
         <meta name="twitter:description" content="Notelabs makes it easy to to write down your thoughts. Choose your devices, and let Notelabs seamlessly adept to them, letting you get back to your writing." />
-        <meta name="twitter:image" content="/social.png"></meta>
+        <meta name="twitter:image" content="https://notelabs.me/social.png"></meta>
       </Head>
       <div>
         <MarketingNav
